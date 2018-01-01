6eae89eac058609bd3129982f88c52a5419b2294
dressel <louisdressel@gmail.com>
2018-01-01 12:43:43 -0800

    added some rudimentary tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,25 @@ using FIB
 using POMDPs
 using POMDPModels
 using POMDPToolbox
-using Base.Test
+using Base.Test     # for @test
 
 pomdp = BabyPOMDP()
 solver = FIBSolver()
 
-@requirements_info solver pomdp
+#@requirements_info solver pomdp
 
+# test that alpha vectors turn out mostly correct
 policy = solve(solver, pomdp)
+alphas = [-29.4557 -36.5093; -19.4557 -16.0629]
+@test isapprox(policy.alphas, alphas, atol=1e-4)
 
+# create uniform belief (0.5,0.5)
+bu = updater(policy)
+b = create_belief(bu)
+
+# check that the action and value functions work
+a = action(policy, b)
+v = value(policy, b)
+
+@test a
+@test isapprox(v, -24.4557, atol=1e-4)
